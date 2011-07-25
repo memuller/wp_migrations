429990cf6ada181256a845d61e668083959aeb67
Matheus E. Muller <hello@memuller.com>
2011-07-25 00:41:22 -0300

    Adds wp-loading function.

--- lib/utils.php
+++ lib/utils.php
@@ -1,5 +1,18 @@
 <?php 
 	
+	function load_wp($domain){
+		// those will simulate a browser HTTP/AJAX request.
+		define('DOING_AJAX', true);
+		define('WP_USE_THEMES', false);
+		$_SERVER = array(
+			'HTTP_HOST' => $domain,
+			'SERVER_NAME' => $domain,
+			'REQUEST_URI' => '/',
+			'REQUEST_METHOD' => 'GET'
+			);
+		require_once("../wp-load.php") ;
+	}
+
 	void function message($text, $line_break = true){
 		$break = $line_break ? "\n" : "" ;
 		echo $text . $break ;
