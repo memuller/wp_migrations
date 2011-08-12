5c306cd8442fcce76ddc01c3a33df17541bc5e15
Matheus E. Muller <hello@memuller.com>
2011-08-12 17:00:34 -0300

    CHanges to the new behavior.

--- import_to_network.php
+++ import_to_network.php
@@ -12,40 +12,31 @@
 	$single_db = new ezSQL_mysql(DB_USER, DB_PASSWORD, $argv[1], DB_HOST);
 	$single_site_url = $single_db->get_var("select option_value from wp_options where option_name = 'home' ") or die('Failed to connect.');
 	$single_site_name = $single_db->get_var(" select option_value from wp_options where option_name = 'blogname' ;");
+	$root_site_domain = $wpdb->get_var(" select domain from wp_site limit 1 ;");
 	
 	message("* Found WP database from $single_site_url ($single_site_name)");
-	$import_type = ask("** Should we import it as a blog or as a site? [blog/site]");
+	$new_path = ask("** Chose a path for the blog (please include backslashes:");
 
-	if($import_type == 'site'){
-		$new_site_domain = ask("** What's the new site address for it? (do NOT use http://)");
-		$new_site_url = 'http://' . $new_site_domain  ;
-		$query = "update wp_options set option_value = 
-			replace(option_value, '$single_site_url', 'http://$new_site_url' ); ";
-		$single_db->query($query);
+	$new_site_url = $root_site_domain . $new_path  ;
+	$query = "update wp_options set option_value = 
+		replace(option_value, '$single_site_url', '$new_site_url' ); ";
+	$single_db->query($query);
+	
+	$new_blog_id = wpmu_create_blog($root_site_domain, $new_path, $single_site_name, 1, null, 1);
 
-		$wpdb->query("insert into wp_site values('', '$new_site_domain', '/' ); ");
-		$new_site_id = $wpdb->get_var("select id from wp_site where domain = '$new_site_domain'");
-		
-		$new_blog_id = wpmu_create_blog($new_site_domain, '/', $single_site_name, 1, null, $new_site_id);
-
-		foreach ($single_db->get_col("show tables",0) as $table_name) {
-			$arr = explode('_', $table_name);
-			array_splice($arr, 1, 0, $new_blog_id);
-			$new_table_name = implode('_', $arr);
-			$single_db->query("create table $new_table_name like $table_name ; ");
-			$single_db->query("insert into $new_table_name select * from $table_name ;");
-			$single_db->query("drop table $table_name");
-		}
-
-		$cmd = sprintf("mysqldump -u %s -p%s -h %s %s > dump.sql", DB_USER, DB_PASSWORD, DB_HOST, $single_db_name);
-		system($cmd) ;
-		$cmd = sprintf("mysql -u %s -p%s -h %s %s < dump.sql", DB_USER, DB_PASSWORD, DB_HOST, DB_NAME);
-		system($cmd);
-
-	}else if($import_type == 'blog'){
-		
-	}else{
-		
+	foreach ($single_db->get_col("show tables",0) as $table_name) {
+		$arr = explode('_', $table_name);
+		array_splice($arr, 1, 0, $new_blog_id);
+		$new_table_name = implode('_', $arr);
+		$single_db->query("create table $new_table_name like $table_name ; ");
+		$single_db->query("insert into $new_table_name select * from $table_name ;");
+		$single_db->query("drop table $table_name");
 	}
+
+	$cmd = sprintf("mysqldump -u %s -p%s -h %s %s > dump.sql", DB_USER, DB_PASSWORD, DB_HOST, $single_db_name);
+	system($cmd) ;
+	$cmd = sprintf("mysql -u %s -p%s -h %s %s < dump.sql", DB_USER, DB_PASSWORD, DB_HOST, DB_NAME);
+	system($cmd);
+
 
  ?>
