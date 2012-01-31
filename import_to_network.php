179c00ef26f775545b4f98e08769c415977b1652
Matheus E. Muller <hello@memuller.com>
2012-01-31 16:52:30 -0200

    Partially functional user migrations.
    * Create user, if one does not already exists w/same email.
    * Migrates all wp_metadata, except wp_capabilities.
    * Assigns authored posts (untested)

--- import_to_network.php
+++ import_to_network.php
@@ -22,16 +22,17 @@
 
 	$new_blog_id = wpmu_create_blog($root_site_domain, $new_path, $single_site_name, 1, null, 1);
 	
-	$new_table_names = array() ; $old_tables = array() ; $new_wp_options_table = "" ;
+	$new_table_names = array() ; $old_tables = array() ; $new_wp_options_table = "" ; $new_posts_table = "" ;
 
 	foreach ($single_db->get_col("show tables",0) as $table_name) {
 		$old_tables[]= $table_name ;
-		$arr = explode('_', $table_name);
+		$arr = explode('_', $table_name); 
 		array_splice($arr, 1, 0, $new_blog_id);
-		$arr[0] = $wpdb->prefix ; 
+		$arr[0] = str_replace("_", "", $wpdb->prefix) ; 
 		$new_table_name = implode('_', $arr);
-
+		
 		if($table_name == 'wp_options') $new_wp_options_table = $new_table_name ;
+		if($table_name == 'wp_posts') $new_posts_table = $new_table_name ;
 
 		if($table_name != 'wp_users' and $table_name != 'wp_usermeta'){
 			$new_table_names[]= $new_table_name ;
@@ -40,27 +41,49 @@
 		}
 	}
 
+	foreach ($single_db->get_results("select * from wp_users", ARRAY_A) as $user) {
+		$old_user_id = $user['ID'] ;
+		foreach (array('spam', 'deleted', 'user_status', 'user_activation_key', 'ID') as $field) {
+			unset($user[$field]);
+		}
+		if( ! $user_id = $wpdb->get_var(sprintf("select id from $wpdb->users where user_email = '%s'", $user['user_email']) )  ){
+			$user_id = wp_insert_user($user) ;
+			wp_insert_user(array('ID'=> $user_id, 'user_pass' => $user['user_pass'])) ;	
+		}
+		
+
+		$metadatas = $single_db->get_results( 
+			"select meta_key, meta_value from wp_usermeta where user_id = $old_user_id " ) ;
+		foreach ($metadatas as $metadata) {
+			if($metadata->meta_key == 'wp_capabilities') continue ;
+			add_user_meta($user_id, $metadata->meta_key, maybe_unserialize($metadata->meta_value), true ) ;
+		}
+
+		$single_db->query(sprintf( 
+				"update $new_posts_table set post_author = %s where author_id = %s", 
+				$user_id, $old_user_id  )) ;
+
+	}
 
 
-
-	$cmd = sprintf( "mysqldump -u %s -p%s -h %s %s %s  %s > dump.sql", 
+	$cmd = sprintf( "mysqldump -u%s -p%s -h %s %s %s  %s > dump.sql", 
 		DB_USER, DB_PASSWORD, DB_HOST, $single_db_name, implode(" ", $new_table_names),
 		$lock_tables ? "" : "--lock-tables=false"
 	);
 	system($cmd) ;
 
-	$cmd = sprintf("mysql -u %s -p%s -h %s %s < dump.sql", 
+	$cmd = sprintf("mysql -u%s -p%s -h %s %s < dump.sql", 
 		DB_USER, DB_PASSWORD, DB_HOST, DB_NAME );
 	system($cmd);
 
 	foreach ($new_table_names as $table_name){
-		$singe_db->query("drop table $table_name ;") ;
+		$single_db->query("drop table $table_name ;") ;
 	}
 
 	$query = "update $new_wp_options_table set option_value =
 		replace(option_value, '$single_site_url', '$new_site_url' ); ";
-	$single_db->query($query);
-
+	$wpdb->query($query);
+	echo ("replaced $single_site_url with $new_site_url") ;
 	$new_domain = ask("** Do you want to map a domain to it? (leave blank if no)") ;
 	if ($new_domain) {
 		$wpdb->query(sprintf("insert into %s values('', $new_blog_id, '$new_domain', 1) ;", $wpdb->prefix . 'domain_mapping' ));
