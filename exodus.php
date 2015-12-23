ad78bf93fdc3eff7ed5f4e74d9fa5fdbe9c7399d
Matheus E. Muller <hello@memuller.com>
2015-12-23 17:40:54 -0200

    Adds base plugin file.

---
+++ exodus.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package Exodus
+ */
+/*
+Plugin Name: Exodus Database Migrations
+Plugin URI: http://github.com/memuller/wp_migrations
+Description: Database backup, exports and migrations for WP Network installs. 
+Version: 15.12a
+Author: Matheus Muller
+Author URI: http://memuller.com
+License: Pending (GPLv2 or later?)
+Text Domain: exodus
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2015 Matheus Muller
+*/
+
+?>
